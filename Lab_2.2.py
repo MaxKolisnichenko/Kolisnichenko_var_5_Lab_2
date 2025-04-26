54d102ca468c52653695b0eed08a5e6b3769bd8a
MaxKolisnichenko <126777664+MaxKolisnichenko@users.noreply.github.com>
2025-04-26 23:20:40 +0300

    Update Lab_2.2.py

--- Lab_2.2.py
+++ Lab_2.2.py
@@ -1,21 +1,28 @@
 #Лаб.2 Рівень 2. Варіант 5.
 
-numbers = list(map(int, input("Введіть цілі числа через пробіл: ").split()))
-
-m = int(input("Введіть кількість рядків: "))
-n = int(input("Введіть кількість стовпців: "))
-
-if len(numbers) != m * n:
-    print("Помилка! Неправильна кількість чисел.")
+input_str = input("Введіть цілі числа через пробіл: ")
+if not all(s.strip('-').isdigit() for s in input_str.split()):
+    print("Помилка! У введених даних повинні бути тільки цілі числа.")
 else:
-    matrix = [numbers[i * n: (i + 1) * n] for i in range(m)]
+    numbers = list(map(int, input_str.split()))
 
-    
-    print("\nПочаткова матриця:")
-    for row in matrix:
-        print(row) # початкова матриця
+    try:
+        m = int(input("Введіть кількість рядків: "))
+        n = int(input("Введіть кількість стовпців: "))
+        
+        if m <= 0 or n <= 0:
+            print("Помилка! Кількість рядків і стовпців повинна бути більшою за 0.")
+        elif len(numbers) != m * n:
+            print("Помилка! Неправильна кількість чисел для матриці розміром", m, "x", n)
+        else:
+            matrix = [numbers[i * n: (i + 1) * n] for i in range(m)]
 
-  
-    print("\nПеревернута матриця:")
-    for row in reversed(matrix):
-        print(row) # перевернута матриця
+            print("\nПочаткова матриця:")
+            for row in matrix:
+                print(row)
+
+            print("\nПеревернута матриця (по горизонталі):")
+            for row in reversed(matrix):
+                print(row)
+    except ValueError:
+        print("Помилка! Введено неціле число для розміру матриці.")
